6445c0910d52722b1eba82b13e6b09a9798076c4
ahmadmolla1995 <ahmadmolla.eng93@gmail.com>
2019-11-26 04:14:23 -0800

    first added

--- Comparator/NameCompare.java
+++
@@ -1,9 +0,0 @@
-package Comparator;
-
-import java.util.Comparator;
-
-public class NameCompare implements Comparator<Movie> {
-    public int compare(Movie first, Movie second) {
-        return first.getName().compareTo(second.getName());
-    }
-}
